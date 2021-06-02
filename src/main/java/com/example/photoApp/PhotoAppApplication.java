fe2b9f3c5a99355e4c78f27b47245e3ab1fb073e
Parth Soni <psparthsoni2@gmail.com>
2021-06-01 21:46:04 -0400

    first commit

---
+++ src/main/java/com/example/photoApp/PhotoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.photoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/photoApp/PhotoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
