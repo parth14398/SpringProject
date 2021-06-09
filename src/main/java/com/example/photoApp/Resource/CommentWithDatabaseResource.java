7cda5245f7c90a318829e62aab42f45ff239d277
Parth Soni <psparthsoni2@gmail.com>
2021-06-09 16:18:17 -0400

    Album crud operation with Database Comment

--- src/main/java/com/example/photoApp/Resource/CommentWithDatabaseResource.java
+++ src/main/java/com/example/photoApp/Resource/CommentWithDatabaseResource.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/comment")
 public class CommentWithDatabaseResource {
     @Autowired
     private CommentWithDatabaseService commentWithDatabaseService;
